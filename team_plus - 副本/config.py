91faae91dee066d8b2c215dd4b99e317097aa92b
ceoluo <40320996+ceoluo@users.noreply.github.com>
2018-06-19 11:25:45 +0800

    Update config.py

--- team_plus - 副本/config.py
+++ team_plus - 副本/config.py
@@ -10,7 +10,7 @@ DB_CONFIG = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
-    'password': 'lx201091',
+    'password': 'xxxxxxxx',
     'db': 'team_plus',
     'charset': 'utf8',
 }
